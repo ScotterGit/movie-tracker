5bb03789d8dd49b299d2b2dad242fd6f34d01158
ScotterGit <70965290+ScotterGit@users.noreply.github.com>
2025-11-09 15:44:48 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -170,7 +170,7 @@ function renderTable() {
       <td>${renderExpandableCell(movie.directors)}</td>
       <td>${renderExpandableCell(movie.writers)}</td>
       <td>${formatDate(movie.dateWatched)}</td>
-      <td>${movie.themesKeywords}</td>
+      <td>${movie.themesKeywords.join(", ")}</td>
     `;
     tbody.appendChild(row);
   });
